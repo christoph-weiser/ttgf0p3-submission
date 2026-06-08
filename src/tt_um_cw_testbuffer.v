a1db799b00e70cc18401835f0a54a6c9efa8eba3
christoph <christoph-weiser@gmx.de>
2026-06-08 19:39:23 +0200

    udpated verilog src file

--- src/tt_um_cw_testbuffer.v
+++ src/tt_um_cw_testbuffer.v
@@ -5,10 +5,10 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_cw_testbuffer (
     input  wire       VGND,
     input  wire       VDPWR,    // 3.3v core power supply
-//    input  wire       VAPWR,    // second analog power supply (VAA)
+    input  wire       VAPWR,    // second analog power supply (VAA)
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
